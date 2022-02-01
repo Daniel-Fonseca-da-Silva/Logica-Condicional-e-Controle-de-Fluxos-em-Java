4df2a98d60ae7f07d89d1f943a5ac30ddb8025f3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-01 02:07:17 -0300

    Criado operadores lógicos

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,43 @@
 public class Main {
 
     public static void main(String[] args) {
-        int i1 = 10;
-        int i2 = 20;
-        float f1 = 4.5F;
-        float f2 = 3.5F;
-        double d1 = 59.6D;
-        char c1 = 'x';
-        char c2 = 'y';
-        String s1 = "Fulano";
-        String s2 = "Fulano";
-        String s3 = "Ciclano";
+
         boolean b1 = true;
         boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
 
-        long l1 = 1597L;
-        long l2 = 8997L;
-        byte y1 = 1;
-        short h1 = 25;
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
 
-        System.out.println("i1 == i2 " + (i1 == i2));
-        System.out.println("i1 != i2 " + (i1 != i2));
-        System.out.println("i1 > i2 " + (i1 > i2));
-        System.out.println("i1 <= i2 " + (i1 <= i2));
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b4 || b4 " + (b4 || b1));
 
-        System.out.println("f1 == f2 " + (f1 == f2));
-        System.out.println("f1 != f2 " + (f1 != f2));
-        System.out.println("f1 >= f2 " + (f1 >= f2));
-        System.out.println("f1 < f2 " + (f1 < f2));
+        System.out.println(!b1);
+        System.out.println(!b2);
 
-        System.out.println("c1 == c2 " + (c1 == c2));
-        System.out.println("c1 != c2 " + (c1 != c2));
-        System.out.println("c1 > c2 " + (c1 > c2));
-        System.out.println("c1 <= c2 " + (c1 <= c2));
+        int i1 = 10;
+        int i2 = 5;
+        float f1 = 20F;
+        float f2 = 50F;
+        System.out.println("((i1 + i2) < (f2 - f1)) && true " + ((i1 + i2) < (f2 - f1) && true));
+        System.out.println("(i1 > i2) || (f2 - f1) " + ((i1 > i2) || (f2 < f1)));
 
-        System.out.println("s1 == s2 " + (s1 == s2));
-        System.out.println("s1 == s3 " + (s1 == s2));
-        System.out.println("s1 != s2 " + (s1 != s2));
-        // System.out.println(s1 >= s2);
-        // System.out.println(s1 < s2);
+        double salarioMensal = 11893.58D;
+        double mediaSalario = 10500D;
 
-        System.out.println("b1 == b2 " + (b1 == b2));
-        System.out.println("b1 != b2 " + (b1 != b2));
-        // System.out.println(b1 > b2);
-        // System.out.println(b1 <= i1);
+        int quantidadeDependentes = 4;
+        int mediaDependentes = 2;
 
-        System.out.println("i2 > f1 " + (i2 > f1));
-        System.out.println("d1 > h1 " + (d1 > h1));
-        // System.out.println(s2 != c1);
-        // System.out.println(s3 != i1);
+        System.out.println(salarioMensal < mediaSalario && (quantidadeDependentes >= mediaDependentes));
 
-        System.out.println("l1 == i2 " + (l1 == i2));
-        System.out.println("l2 == i1 " + (l2 == i1));
-        System.out.println("y1 != h1 " + (y1 == h1));
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes = quantidadeDependentes > mediaDependentes;
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+        System.out.println("recebeAuxilio " + recebeAuxilio);
 
     }
 
